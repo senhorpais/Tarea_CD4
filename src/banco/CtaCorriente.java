830896c443c89d8ed39e18858e281e094f071900
crist <crist@LAPTOP-B73I42DC>
2024-03-04 09:46:52 +0100

    Commit después de refactorización y análisis del código con SonarLint

---
+++ src/banco/CtaCorriente.java
@@ -0,0 +1,90 @@
+package banco;
+
+public class CtaCorriente {
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the cuenta
+     */
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    /**
+     * @param cuenta the cuenta to set
+     */
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    /**
+     * @return the saldocuenta
+     */
+    public double getSaldocuenta() {
+        return saldocuenta;
+    }
+
+    /**
+     * @param saldocuenta the saldocuenta to set
+     */
+    public void setSaldocuenta(double saldocuenta) {
+        this.saldocuenta = saldocuenta;
+    }
+
+    /**
+     * @return the interes
+     */
+    public double getInteres() {
+        return interes;
+    }
+
+    /**
+     * @param interes the interes to set
+     */
+    public void setInteres(double interes) {
+        this.interes = interes;
+    }
+    private String nombre;
+    private String cuenta;
+    private double saldocuenta;
+    private double interes;
+
+    public CtaCorriente()
+    {
+    }
+    public CtaCorriente(String nombre, String cuenta, double saldo, double interes)
+    {
+        this.nombre = nombre;
+        this.cuenta = cuenta;
+        this.saldocuenta = saldo;
+        this.interes = interes;
+    }
+    public void ingresar(double cantidad)
+    {
+        if (cantidad<0)
+            throw new IllegalArgumentException("No se puede ingresar una cantidad negativa");
+        setSaldocuenta(getSaldocuenta() + cantidad);
+    }
+
+    public void retirar(double cantidad)
+    {
+        if (cantidad <= 0)
+            throw new IllegalArgumentException ("No se puede retirar una cantidad negativa");
+        if (getSaldocuenta()< cantidad)
+            throw new IllegalArgumentException ("No se hay suficiente saldo");
+        setSaldocuenta(getSaldocuenta() - cantidad);
+    }
+}
--- src/tareacd4/Main.java
+++ src/tareacd4/Main.java
@@ -1,15 +1,21 @@
 package tareacd4;
 
+import banco.CtaCorriente;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        CC miCuenta;
-        double saldoActual;
+        operativa_cuenta();
+    }
 
-        miCuenta = new CC("Koldo García Ábalos","0001-2345-07-1234567890",5000,0);
-        saldoActual = miCuenta.estado();
+    public static void operativa_cuenta() {
+        CtaCorriente miCuenta;
+        double saldoActual;
+        
+
+        miCuenta = new CtaCorriente("Koldo García Ábalos","0001-2345-07-1234567890",5000,0);
+        saldoActual = miCuenta.getSaldocuenta();
         System.out.println("El saldo inicial es de "+ saldoActual +" euros");
 
         try {
@@ -17,7 +23,7 @@ public class Main {
             System.out.println("¿Cuánto desea retirar?");
             double importe = entrada.nextDouble();
             miCuenta.retirar(importe);
-            System.out.println("El saldo actual de su cuenta es "+miCuenta.estado()+" euros");
+            System.out.println("El saldo actual de su cuenta es "+miCuenta.getSaldocuenta()+" euros");
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
@@ -26,7 +32,7 @@ public class Main {
             System.out.println("¿Cuánto desea ingresar en su cuenta?");
             double importe = entrada.nextDouble();
             miCuenta.ingresar(importe);
-            System.out.println("El saldo actual de su cuenta es "+miCuenta.estado()+" euros");
+            System.out.println("El saldo actual de su cuenta es "+miCuenta.getSaldocuenta()+" euros");
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
